c7d31bcd55e35c59861751f27b90c74bec6acf21
Matthew Begnoche <132032766+mattbegnoche@users.noreply.github.com>
2024-03-26 22:01:56 -0500

    Update games.js

--- games.js
+++ games.js
@@ -22,41 +22,39 @@ export default function handleGames() {
 
   if (window.innerWidth > MOBILE_WIDTH) {
     // Desktop Version
-    phoneAnimation.to(".phone__game.is-slay", { x: "-92%", y: "-205%" }, 0);
-    phoneAnimation.to(".phone__game.is-green", { x: "29%", y: "-230%" }, "<");
-    phoneAnimation.to(".phone__game.is-blue", { x: "108%", y: "-107%" }, "<");
-    phoneAnimation.to(".phone__game.is-orange", { x: "-182%", y: "14%" }, "<");
-    phoneAnimation.to(".phone__game.is-forager", { x: "113%", y: "104%" }, "<");
+    phoneAnimation.to(".phone__game.is-top-left", { x: "-92%", y: "-205%" }, 0);
     phoneAnimation.to(
-      ".phone__game.is-unpacking",
-      { x: "-100%", y: "90%" },
+      ".phone__game.is-top-right",
+      { x: "29%", y: "-230%" },
       "<"
     );
-    phoneAnimation.to(".phone__game.is-wizard", { x: "-162%", y: "-95%" }, "<");
     phoneAnimation.to(
-      ".phone__game.is-crying-sun",
-      { x: "160%", y: "-5%" },
+      ".phone__game.is-bottom-left",
+      { x: "108%", y: "-107%" },
       "<"
     );
-    phoneAnimation.to(".svg.cc-beams", { opacity: 1 });
+    phoneAnimation.to(
+      ".phone__game.is-bottom-right",
+      { x: "-182%", y: "14%" },
+      "<"
+    );
   } else {
     // Mobile Version
-    phoneAnimation.to(".phone__game.is-slay", { x: "-98%", y: "-231%" }, 0);
-    phoneAnimation.to(".phone__game.is-green", { x: "155%", y: "-41%" }, "<");
-    phoneAnimation.to(".phone__game.is-blue", { x: "48%", y: "-194%" }, "<");
-    phoneAnimation.to(".phone__game.is-orange", { x: "-176%", y: "104%" }, "<");
-    phoneAnimation.to(".phone__game.is-forager", { x: "74%", y: "232%" }, "<");
+    phoneAnimation.to(".phone__game.is-top-left", { x: "-98%", y: "-231%" }, 0);
     phoneAnimation.to(
-      ".phone__game.is-unpacking",
-      { x: "-89%", y: "221%" },
+      ".phone__game.is-top-right",
+      { x: "155%", y: "-41%" },
       "<"
     );
-    phoneAnimation.to(".phone__game.is-wizard", { x: "-154%", y: "-62%" }, "<");
     phoneAnimation.to(
-      ".phone__game.is-crying-sun",
-      { x: "149%", y: "92%" },
+      ".phone__game.is-bottom-left",
+      { x: "48%", y: "-194%" },
       "<"
     );
-    phoneAnimation.to(".svg.cc-beams", { opacity: 1 });
+    phoneAnimation.to(
+      ".phone__game.is-bottom-right",
+      { x: "-176%", y: "104%" },
+      "<"
+    );
   }
 }
